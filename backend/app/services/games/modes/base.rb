848972ee1278f68a099c2cf5c20dfce7e1348345
bartek <bartosztra@gmail.com>
2026-05-08 19:26:51 +0200

    .

--- backend/app/services/games/modes/base.rb
+++ backend/app/services/games/modes/base.rb
@@ -2,9 +2,14 @@ module Games
   module Modes
     class Base
       WIN_COMBINATIONS = [
-        [ 0, 1, 2 ], [ 3, 4, 5 ], [ 6, 7, 8 ],
-        [ 0, 3, 6 ], [ 1, 4, 7 ], [ 2, 5, 8 ],
-        [ 0, 4, 8 ], [ 2, 4, 6 ]
+        [ 0, 1, 2 ], 
+        [ 3, 4, 5 ], 
+        [ 6, 7, 8 ],
+        [ 0, 3, 6 ], 
+        [ 1, 4, 7 ], 
+        [ 2, 5, 8 ],
+        [ 0, 4, 8 ], 
+        [ 2, 4, 6 ]
       ].freeze
 
       def initialize(user:, game:, cell:)
